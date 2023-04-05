51d09051948ee3903c1abf1c54628dbfc952d34f
ieienunes <42755033+ieienunes@users.noreply.github.com>
2023-04-05 10:58:12 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,20 @@
+#    Copyright (c) 2021 Ayush
+#    
+#    This program is free software: you can redistribute it and/or modify  
+#    it under the terms of the GNU General Public License as published by  
+#    the Free Software Foundation, version 3.
+# 
+#    This program is distributed in the hope that it will be useful, but 
+#    WITHOUT ANY WARRANTY; without even the implied warranty of 
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+#    General Public License for more details.
+# 
+#    License can be found in < https://github.com/Ayush7445/telegram-auto_forwarder/blob/main/License > .
+
 from telethon import TelegramClient, events
 from decouple import config
 import logging
 from telethon.sessions import StringSession
-from bs4 import BeautifulSoup
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
 
@@ -29,22 +41,10 @@ except Exception as ap:
 async def sender_bH(event):
     for i in TO:
         try:
-            message = event.message
-            original_message = None
-            # Check if the message is forwarded
-            if message.forward:
-                # Get the original message
-                original_message = await message.get_reply_message()
-            # Otherwise, the message is not forwarded
-            else:
-                # The original message is the same as the message received
-                original_message = message
-            
-            # Modify the original message to include the new link
-            original_message.text = original_message.text.replace("https://cutt.ly/BBRbet", "https://www.bbrbet.com/c-lnkl32RW?lang=pt")
-            
-            # Forward the modified message to the target chat
-            await BotzHubUser.send_message(i, original_message)
+            await BotzHubUser.send_message(
+                i,
+                event.message
+            )
         except Exception as e:
             print(e)
 
