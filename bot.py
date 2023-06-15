e84211c4eeeaf2cf054fa98a9cad1e34340f77a3
ieienunes <42755033+ieienunes@users.noreply.github.com>
2023-06-15 10:36:24 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,10 @@ from telethon import TelegramClient, events
 from decouple import config
 import logging
 from telethon.sessions import StringSession
-from bs4 import BeautifulSoup
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
 
-print("Starting...")
+print("ATIVANDO...")
 
 # Basics
 APP_ID = config("APP_ID", default=None, cast=int)
@@ -25,11 +24,19 @@ except Exception as ap:
     print(f"ERROR - {ap}")
     exit(1)
 
-@BotzHubUser.on(events.NewMessage(incoming=True, chats=FROM))
+@BotzHubUser.on(events.NewMessage(incoming=True, chats=FROM, pattern=r'(?i)(?!.*✅✅✅ GREEN ✅✅✅)'))
 async def sender_bH(event):
+    message = event.message
+    # substituir textos específicos antes de encaminhar a mensagem
+    if '➡️ ENTRE AQUI : https://fwd.cx/HMY5zeG8hZYa' in message.text:
+        message.text = message.text.replace('➡️ ENTRE AQUI : https://fwd.cx/HMY5zeG8hZYa', '👉🏻[CRIE SUA CONTA AQUI](https://www.bbrbet.com/?p=lnkl32RW&lang=pt)👈🏻')
+    if '🟢🟢Entrada Confirmada🟢🟢' in message.text:
+        message.text = message.text.replace('🟢🟢Entrada Confirmada🟢🟢', '**✅ OPORTUNIDADE IDENTIFICADA**')
+        
+        
+    # Loop over the target chats
     for i in TO:
         try:
-            message = event.message
             original_message = None
             # Check if the message is forwarded
             if message.forward:
@@ -41,64 +48,12 @@ async def sender_bH(event):
                 original_message = message
 
             # Modify the original message to include the new link
-            original_message.text = original_message.text.replace("https://cutt.ly/Brabet", "https://www.bbrbet.com/c-lnkl32RW?lang=pt")
-                                                                   
-            # Forward the modified message to the target chat
-            await BotzHubUser.send_message(i, original_message)
-        except Exception as e:
-            print(e)
-
-print("Bot has started.")
-BotzHubUser.run_until_disconnected()
-from telethon import TelegramClient, events
-from decouple import config
-import logging
-from telethon.sessions import StringSession
-from bs4 import BeautifulSoup
-
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
-
-print("Starting...")
-
-# Basics
-APP_ID = config("APP_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-SESSION = config("SESSION")
-FROM_ = config("FROM_CHANNEL")
-TO_ = config("TO_CHANNEL")
-
-FROM = [int(i) for i in FROM_.split()]
-TO = [int(i) for i in TO_.split()]
-
-try:
-    BotzHubUser = TelegramClient(StringSession(SESSION), APP_ID, API_HASH)
-    BotzHubUser.start()
-except Exception as ap:
-    print(f"ERROR - {ap}")
-    exit(1)
-
-@BotzHubUser.on(events.NewMessage(incoming=True, chats=FROM))
-async def sender_bH(event):
-    for i in TO:
-        try:
-            message = event.message
-            original_message = None
-            # Check if the message is forwarded
-            if message.forward:
-                # Get the original message
-                original_message = await message.get_reply_message()
-            # Otherwise, the message is not forwarded
-            else:
-                # The original message is the same as the message received
-                original_message = message
-
-            # Modify the original message to include the new link
-            original_message.text = original_message.text.replace("https://cutt.ly/BBRbet", "https://www.bbrbet.com/c-lnkl32RW?lang=pt")
+            original_message.text = original_message.text.replace("https://fwd.cx/lmBBuPRNuDaQ", "https://cutt.ly/criar_conta_bbrbet")
 
             # Forward the modified message to the target chat
             await BotzHubUser.send_message(i, original_message)
         except Exception as e:
             print(e)
 
-print("Bot has started.")
+print("BOT INICIADO.")
 BotzHubUser.run_until_disconnected()
